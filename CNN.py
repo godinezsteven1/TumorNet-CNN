8f3d00bfe5baf9736c613d25b7be1947a99ebc7d
godinezsteven1 <steven.godin.42@gmail.com>
2025-04-28 13:34:16 -0400

    finished forward algorithm for cnn

--- CNN.py
+++ CNN.py
@@ -1,23 +1,25 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 # lets make a basic CNN neural network as of now and change it based on needs 
 
 # MRI scans are in black and white so we can use a gray scale channel instead of 3 (rgb)
-
+# --------------Convolution parameters ---------------------------
 grayscaleChannel = 1 # -  image input channel -- 1 input for grayscale 
 out_channels = 64 # this isnt a MNSIT + Need complex features to be learned 
 kernel_size = 3 # cannot go wrong w 3 
 imagePadding = 1 # works with kernel size 3 
 stride = 2 # idea for downsampling with out to much overlap 
+# --------------Neural Network Parameters ------------------------
 inputFeature = 256 * 6 * 6 # approx 9k features 
 outputClasses = 4 
 neuronsLayer1 = 1024
 neuronsLayer2 = 256
-# 9216 --> 1024 --> 256 --> 4 
 p = 0.2
-
+# 9216 --> 1024 --> 256 --> 4 
+# -----------------------------------------------------------------
 
 
 # nn.module - base class for all neural networks -- model needs to subclass 
@@ -54,5 +56,39 @@ class CNN_Net(nn.Module):
         self.adaptivePool = nn.AdaptiveAvgPool2d((6, 6))
         self.hiddenLayer1 = nn.Linear(inputFeature, neuronsLayer1)
         self.hiddenLayer2 = nn.Linear(neuronsLayer1, neuronsLayer2)
-        self.hiddenLayer3 = nn.Linear(neuronsLayer2, outputClasses)
+        self.output = nn.Linear(neuronsLayer2, outputClasses)
         self.dropout = nn.Dropout(p)
+
+    def forward(self, x):
+        x = self.conv1Layer(x)
+        # because it is a 4 class clasification going to relu to keep x values 
+        x = F.relu(x)
+        x = self.pool(x) # reduce dim
+
+        x = self.conv2Layer(x)
+        x = F.relu(x) 
+        x = self.pool(x)
+
+        x = self.conv3Layer(x)
+        x = F.relu(x)
+        x = self.pool(x) 
+
+        # applied 3 convolutional layers -- now apply connected layers 
+        # flatten features 
+        x = x.view(-1, inputFeature)
+
+        x = self.hiddenLayer1(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.hiddenLayer2(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.output(x)
+
+        return x 
+        
+
+# still need to apply a cross entoorpu loss or a  softmax maybe in the visuals 
+# cel easier i beleive pytorch version applies a softmax at the end. 
