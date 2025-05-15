6b4d09c2d77ac76b600933c72df40d08056d3544
godinezsteven1 <steven.godin.42@gmail.com>
2025-05-14 21:17:16 -0500

    added live app with dummy variables #2

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.write("# Brain Tumor Scanner- Convolutional Neural Network Scan")
+st.set_page_config(layout="wide")
 
+st.title("Convolutional Neural Network Scan: Brain Tumor Scanner - ")
+st.subheader("Model Analytics Dashboard")
+
+data = np.random.randn(1, 2)
+df = pd.DataFrame(data, columns=["Accuracy over time", "Training loss over epochs"])
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Model Accuracy & Loss")
+    st.line_chart(df, use_container_width=True)
+
+x-col2 = 'Type'
+with col2:
+    st.subheader("Tumor Prediction Distribution")
+    data = pd.DataFrame({
+        x-col2: ['Glioma', 'Meningioma', 'Tumor', 'Healthy'],
+        'Count': [1, 1, 1, 1]
+    })
+    st.bar_chart(data.set_index(x-col2), use_container_width=True)  
