6afa864c32143c0eea5a47f156f552892932822e
godinezsteven1 <steven.godin.42@gmail.com>
2025-06-09 22:33:06 -0500

    fixed syntax to compile

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ import pandas as pd
 import numpy as np
 
 st.set_page_config(layout="wide")
-
 st.title("Convolutional Neural Network Scan: Brain Tumor Scanner - ")
 st.subheader("Model Analytics Dashboard")
 
 data = np.random.randn(1, 2)
 df = pd.DataFrame(data, columns=["Accuracy over time", "Training loss over epochs"])
-
 
 col1, col2 = st.columns(2)
 
@@ -17,11 +15,13 @@ with col1:
     st.subheader("Model Accuracy & Loss")
     st.line_chart(df, use_container_width=True)
 
-x-col2 = 'Type'
+
+type_col = 'Type'
+
 with col2:
     st.subheader("Tumor Prediction Distribution")
     data = pd.DataFrame({
-        x-col2: ['Glioma', 'Meningioma', 'Tumor', 'Healthy'],
+        type_col: ['Glioma', 'Meningioma', 'Pituitary', 'No Tumor'],
         'Count': [1, 1, 1, 1]
     })
-    st.bar_chart(data.set_index(x-col2), use_container_width=True)  
+    st.bar_chart(data.set_index(type_col), use_container_width=True)
